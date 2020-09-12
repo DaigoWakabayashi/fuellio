ce2d253513edbc702c9744af84b4ae92511e063f
daigo-program <mamushi.journey@gmail.com>
2020-09-12 14:21:12 +0900

    要らないもの削除、AppBarだけの状態

---
+++ lib/input_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Fuellio'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+          ],
+        ),
+      ),
+    );
+  }
+}
